890892e30bf760d50fb23f6b925590ff4ea92a28
André <andre.escocard@nttdata.com>
2023-02-23 10:11:07 -0300

    feat: use .env to store API key

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,10 +2,10 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
