d4f7e3313e8d72b7c580eb08e2f4482ca002e081
André <andre.escocard@hesea.com.br>
2023-03-17 13:49:44 -0300

    feat: fav or unfav movie

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -3,6 +3,9 @@ import { AxiosResponse } from 'axios';
 import { Observable } from 'rxjs';
 import { AppService } from './app.service';
 import { Movie } from './interface/movie';
+import { LocalStorage } from 'node-localstorage';
+
+
 
 @Controller()
 export class AppController {
@@ -16,5 +19,33 @@ export class AppController {
   @Get('movie/:moviename')
   findMovie(@Param() params) {
     return this.appService.findMovie(params.moviename);
+  }
+
+  @Get('favunfav/:imdbID')
+  favOrUnfavMovie(@Param() params) {
+    
+    const localStorage = new LocalStorage('./scratch');
+    
+
+    const stored = localStorage.getItem('imbdIDs');
+    if (stored === null || typeof(stored) == 'undefined') {
+      localStorage.setItem('imbdIDs', [])
+    } else {
+      let old_Data = [];
+      if(stored != ""){
+         old_Data = JSON.parse(stored);
+         if(old_Data.includes(params.imdbID)){
+          old_Data.splice(old_Data.indexOf(params.imdbID), 1);
+          localStorage.setItem('imbdIDs', JSON.stringify(old_Data));
+         }else{
+          old_Data.push(params.imdbID);
+          localStorage.setItem('imbdIDs', JSON.stringify(old_Data));
+         }
+      }
+      
+
+    }
+
+    return JSON.parse(localStorage.getItem('imbdIDs'));
   }
 }
--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -4,11 +4,14 @@ import { AxiosResponse } from 'axios';
 import { map, Observable } from 'rxjs';
 import { Movie } from './interface/movie';
 
+
 @Injectable()
 export class AppService {
   constructor(private readonly httpService: HttpService) {}
 
   private readonly movie: Movie[] = [];
+
+  //http://www.omdbapi.com/?i=tt0499549 @todo list favs
   
   getAPIKey(): string {
     let apiKey = process.env.APIKEY;
@@ -16,10 +19,13 @@ export class AppService {
   }
 
   findMovie(movieName) {
+    //http://localhost:3000/movie/avatar
     let apiKey = this.getAPIKey();
     const url = "https://www.omdbapi.com/?apikey="+apiKey+"&s="+movieName;
     return this.httpService.get(url).pipe(
       map(response => response.data)
     );
   }
+
+
 }
