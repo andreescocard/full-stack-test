b3a729f068573e1d34f2c39853e5e66567979b25
André <andre.escocard@hesea.com.br>
2023-03-20 10:41:09 -0300

    style: adjust footer adding margins

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Grid } from '@ui5/webcomponents-react';
 
 const Footer = () => {
   return (
-    <Grid position="Center" className="footerRow text-center">
+    <Grid position="Center" className="footerRow text-center mtb-20">
         <div data-layout-span="XL12 L12 M12 S12">
           2023 - Created by André Escocard
         </div>
